93884d1c5840404404e39175f208742f91e2788e
igiza1213 <igiza1213@gmail.com>
2024-01-27 07:09:18 +0900

    c

--- website/app/(root)/_components/navbar.tsx
+++ website/app/(root)/_components/navbar.tsx
@@ -1,29 +1,32 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="h-[90px] bg-black/30 flex px-[120px] pb-[20px] pt-[30px] justify-between">
+    <div className="absolute top-0 left-0 w-full h-[90px] bg-black/30 flex px-[120px] pb-[20px] pt-[30px] justify-between z-50 ">
       <div className="flex gap-[12px] justify-center items-center">
         <Image src={"/images/logo.webp"} alt={"logo"} width={40} height={40} />
         <span className="text-[28px] font-extrabold text-white flex items-center justify-center leading-[1.2em]">
           KSDC
         </span>
       </div>
-      <div className="flex justify-center items-center gap-[68px]">
+      <div className="flex justify-center items-center gap-[68px] mb:hidden">
         <div className="text-white text-[20px] font-light leading-[1.2em]">
-          VISION
+          <Link href={"#vision"} className="">
+            VISION
+          </Link>
         </div>
         <div className="text-white text-[20px] font-light leading-[1.2em]">
-          ORGANIZER
+          <Link href={"#organizer"}>ORGANIZER</Link>
         </div>
         <div className="text-white text-[20px] font-light leading-[1.2em]">
-          HISTORY
+          <Link href={"#history"}>HISTORY</Link>
         </div>
         <div className="text-white text-[20px] font-light leading-[1.2em]">
-          EVENTS
+          <Link href={"#events"}>EVENTS</Link>
         </div>
         <div className="text-white text-[20px] font-light leading-[1.2em]">
-          C. O. C
+          <Link href={"#coc"}>C. O. C</Link>
         </div>
       </div>
     </div>
--- website/app/(root)/page.tsx
+++ website/app/(root)/page.tsx
@@ -4,92 +4,99 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="h-[calc(100vh-90px)] relative w-full">
-        <div className="absolute top-[-90px] h-[100vh] -z-10 bg-black/100 flex justify-center items-center w-full]">
+      <div className="h-[calc(100vh-90px)] relative w-full mb:h-[calc(100vh-2px)]">
+        <div className="absolute top-[-90px] h-[100vh] z-0 bg-black/100 flex justify-center items-center w-full] mb:left-[-902px] mb:top-[-253px] mb:h-[1097px]">
           <img
             src={"/images/background.webp"}
             alt={""}
-            className="object-center object-cover overflow-clip w-[calc(100vw + 3000px)]"
+            className="object-center object-cover overflow-clip w-[calc(100vw + 3000px)] mb:h-full"
           />
         </div>
-        <div className="absolute top-[calc(50%-90px)] left-[50%] z-10 w-min h-[800px] -translate-x-[50%] -translate-y-[50%] pb-[30px] flex items-center justify-end flex-col gap-[280px]">
+        <div className="absolute top-[calc(50%-90px)] left-[50%] z-10 w-min h-[800px]  -translate-x-[50%] -translate-y-[50%] pb-[30px] flex items-center justify-end flex-col gap-[280px] mb:gap-[300px] mb:h-[100vh] mb:w-full mb:transform-none mb:top-[calc(50%-100vh/2)] mb:left-[calc(50%-100%/2)]">
           <div className="flex items-center justify-start flex-col">
             <div className="flex items-center">
-              <span className="text-[72px] font-extrabold text-white leading-[1.2em]">
+              <span className="text-[72px] font-extrabold text-white leading-[1.2em] mb:text-[60px]">
                 KSDC
               </span>
             </div>
             <div className="flex items-center">
-              <span className="text-[32px] font-normal text-white leading-[1.2em]">
+              <span className="text-[32px] font-normal text-white leading-[1.2em] mb:text-[20px]">
                 Korea Student Developer Club
               </span>
             </div>
           </div>
-          <div className="flex items-center gap-[28px] justify-center">
-            <div className="flex justify-start flex-col">
-              <span className="text-[20px] leading-[27px] text-white font-normal">
-                <Link
-                  className="underline"
-                  href={"https://festa.io/hosts/2003"}
-                >
-                  FESTA
-                </Link>
-              </span>
+          <div className="flex items-center gap-[10px] justify-center mb:gap-[10px] mb:flex-col mb:justify-center mb:items-center">
+            <div className="flex gap-[10px] mb:w-full mb:justify-center">
+              <div className="flex justify-start flex-col">
+                <span className="text-[20px] leading-[27px] text-white font-normal">
+                  <Link
+                    className="underline"
+                    href={"https://festa.io/hosts/2003"}
+                  >
+                    FESTA
+                  </Link>
+                </span>
+              </div>
+              <div className="flex justify-start flex-col">
+                <span className="text-[20px] leading-[27px] text-white font-normal">
+                  <Link
+                    className="underline"
+                    href={"https://github.com/Korea-Student-Developer-Club"}
+                  >
+                    GITHUB
+                  </Link>
+                </span>
+              </div>
             </div>
-            <div className="flex justify-start flex-col">
-              <span className="text-[20px] leading-[27px] text-white font-normal">
-                <Link
-                  className="underline"
-                  href={"https://github.com/Korea-Student-Developer-Club"}
-                >
-                  GITHUB
-                </Link>
-              </span>
-            </div>
-            <div className="flex justify-start flex-col">
-              <span className="text-[20px] leading-[27px] text-white font-normal">
-                <Link
-                  className="underline"
-                  href={"https://www.instagram.com/ksdc.io/"}
-                >
-                  INSTAGRAM
-                </Link>
-              </span>
-            </div>
-            <div className="flex justify-start flex-col">
-              <span className="text-[20px] leading-[27px] text-white font-normal">
-                <Link
-                  className="underline"
-                  href={"https://open.kakao.com/o/gGAJxvvf"}
-                >
-                  KAKAOTALK
-                </Link>
-              </span>
-            </div>
-            <div className="flex justify-start flex-col">
-              <span className="text-[20px] leading-[27px] text-white font-normal">
-                <Link
-                  className="underline"
-                  href={"https://discord.com/invite/5j3STkjprb"}
-                >
-                  DISCORD
-                </Link>
-              </span>
+            <div className="flex gap-[10px] mb:w-full">
+              <div className="flex justify-start flex-col">
+                <span className="text-[20px] leading-[27px] text-white font-normal">
+                  <Link
+                    className="underline"
+                    href={"https://www.instagram.com/ksdc.io/"}
+                  >
+                    INSTAGRAM
+                  </Link>
+                </span>
+              </div>
+              <div className="flex justify-start flex-col">
+                <span className="text-[20px] leading-[27px] text-white font-normal">
+                  <Link
+                    className="underline"
+                    href={"https://open.kakao.com/o/gGAJxvvf"}
+                  >
+                    KAKAOTALK
+                  </Link>
+                </span>
+              </div>
+              <div className="flex justify-start flex-col">
+                <span className="text-[20px] leading-[27px] text-white font-normal">
+                  <Link
+                    className="underline"
+                    href={"https://discord.com/invite/5j3STkjprb"}
+                  >
+                    DISCORD
+                  </Link>
+                </span>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="flex items-center justify-center px-[142px] py-[36px] bg-[#3c3c3c]">
+      <div className="flex items-center justify-center px-[142px] py-[36px] bg-[#3c3c3c] mb:px-[10px]">
         <div className="flex justify-center flex-col">
-          <p className="text-[24px] text-white font-normal text-center">
+          <p className="text-[24px] text-white font-normal text-center mb:text-[16px]">
             배움을 지향하시는 모든 분들이 정보를 교류하고,
           </p>
-          <p className="text-[24px] text-white font-normal text-center">
+          <p className="text-[24px] text-white font-normal text-center mb:text-[16px]">
             커뮤니케이션할 수 있는 커뮤니티를 만들어가고 있습니다!
           </p>
         </div>
       </div>
-      <div className="py-[120px] flex justify-start items-center flex-col gap-[56px]">
+      <div
+        className="py-[120px] flex justify-start items-center flex-col gap-[56px]"
+        id="vision"
+      >
         <div className="flex flex-col gap-[8px] justify-start">
           <div className="flex justify-center flex-col">
             <span className="text-[36px] font-extrabold text-center">
@@ -107,16 +114,22 @@ export default function Home() {
             <img src="/images/logo.webp" alt="logo" />
           </div>
           <div className="flex justify-center flex-col leading-[1.2em]">
-            <span className="text-[40px] font-bold">“함께하는 성장”</span>
+            <span className="text-[40px] font-bold mb:text-[35px]">
+              “함께하는 성장”
+            </span>
           </div>
           <div className="">
-            <span className="text-[20px] font-normal text-center">
-              성장을 위한 사람들의 커뮤니티가 되도록 언제나 노력할 것입니다!
+            <span className="text-[20px] font-normal text-center mb:text-[18px]">
+              성장을 위한 사람들의 커뮤니티가 되도록
+              <div>언제나 노력할 것입니다!</div>
             </span>
           </div>
         </div>
       </div>
-      <div className="flex items-center justify-start flex-col py-[120px] gap-[56px]">
+      <div
+        className="flex items-center justify-start flex-col py-[120px] gap-[56px]"
+        id="organizer"
+      >
         <div className="flex items-center justify-start flex-col gap-[8px]">
           <span className="text-[36px] font-extrabold leading-[1.2em]">
             ORGANIZER
@@ -126,39 +139,42 @@ export default function Home() {
           </span>
         </div>
         <div className="flex justify-start items-center gap-[24px] flex-col">
-          <div className="flex items-center justify-center gap-[48px]">
-            <span className="text-[32px] font-extrabold leading-[1.2em]">
+          <div className="flex items-center justify-center gap-[48px] mb:gap-[36px]">
+            <span className="text-[32px] font-extrabold leading-[1.2em] mb:text-[24px]">
               정현서
             </span>
-            <span className="text-[32px] font-extrabold leading-[1.2em]">
+            <span className="text-[32px] font-extrabold leading-[1.2em] mb:text-[24px]">
               강모민
             </span>
-            <span className="text-[32px] font-extrabold leading-[1.2em]">
+            <span className="text-[32px] font-extrabold leading-[1.2em] mb:text-[24px]">
               김마유
             </span>
           </div>
-          <div className="flex items-center justify-center gap-[48px]">
-            <span className="text-[32px] font-extrabold leading-[1.2em]">
+          <div className="flex items-center justify-center gap-[48px] mb:gap-[36px]">
+            <span className="text-[32px] font-extrabold leading-[1.2em] mb:text-[24px]">
               김민재
             </span>
-            <span className="text-[32px] font-extrabold leading-[1.2em]">
+            <span className="text-[32px] font-extrabold leading-[1.2em] mb:text-[24px]">
               국재윤
             </span>
-            <span className="text-[32px] font-extrabold leading-[1.2em]">
+            <span className="text-[32px] font-extrabold leading-[1.2em] mb:text-[24px]">
               이예흔
             </span>
-            <span className="text-[32px] font-extrabold leading-[1.2em]">
+            <span className="text-[32px] font-extrabold leading-[1.2em] mb:text-[24px]">
               이정현
             </span>
           </div>
         </div>
         <div className="flex justify-center">
-          <span className="text-[20px] font-normal leading-[1.2em]">
-            현재 일곱 분의 오거나이저 분들로 이루어져 있습니다.
+          <span className="text-[20px] font-normal leading-[1.2em] text-center">
+            현재 일곱 분의 오거나이저 분들로 <div>이루어져 있습니다.</div>
           </span>
         </div>
       </div>
-      <div className="flex items-center justify-start flex-col py-[120px] gap-[56px]">
+      <div
+        className="flex items-center justify-start flex-col py-[120px] gap-[56px]"
+        id="history"
+      >
         <div className="flex items-center justify-start flex-col gap-[8px]">
           <span className="text-[36px] font-extrabold leading-[1.2em]">
             HISTORY
@@ -186,7 +202,10 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="flex items-center justify-start flex-col py-[120px] gap-[56px] overflow-x-hidden">
+      <div
+        className="flex items-center justify-start flex-col py-[120px] gap-[56px] overflow-x-hidden"
+        id="events"
+      >
         <div className="flex items-center justify-start flex-col gap-[8px]">
           <span className="text-[36px] font-extrabold leading-[1.2em]">
             EVENTS
@@ -200,13 +219,14 @@ export default function Home() {
             <div className="text-[28px] font-extrabold tracking-[0.05em] leading-[1.2em]">
               HYPER
             </div>
-            <div className="text-[20px] font-normal tracking-[0.05em] leading-[1.2em]">
-              KSDC에서 진행하는 전반적 기술분야에 관한 컨퍼런스입니다.
+            <div className="text-[20px] font-normal tracking-[0.05em] leading-[1.2em] text-center">
+              KSDC에서 진행하는
+              <div>전반적 기술분야에 관한 컨퍼런스입니다.</div>
             </div>
             <div className="h-[449px] w-full">
               <section className="flex justify-center items-center relative">
                 <div className="gap-[10px] w-[calc(601px+601px+601px+20px)] flex items-center justify-center absolute top-2 place-items-center ">
-                  <div className="">
+                  <div className="mb:hidden">
                     <img
                       src="/images/left.webp"
                       alt=""
@@ -220,7 +240,7 @@ export default function Home() {
                       className="h-[400px] w-[601px] object-cover"
                     />
                   </div>
-                  <div className="">
+                  <div className="mb:hidden">
                     <img
                       src="/images/right.webp"
                       alt=""
@@ -254,7 +274,10 @@ export default function Home() {
           </Link>
         </div>
       </div>
-      <div className="flex items-center justify-start flex-col py-[120px] px-[300px] gap-[56px]">
+      <div
+        className="flex items-center justify-start flex-col py-[120px] px-[300px] gap-[56px] mb:px-[50px] mb:py-[100px]"
+        id="coc"
+      >
         <div className="flex items-center justify-start gap-[8px] flex-col">
           <div className="font-extrabold tracking-[0.05em] leading-[1.2em] text-[36px]">
             C. O. C
@@ -323,10 +346,10 @@ export default function Home() {
         </div>
       </div>
       <footer className="flex items-center justify-center gap-[24px] flex-col bg-[#3c3c3c] pt-[24px] pb-[50px]">
-        <div className="text-[18px] leading-[27px] text-[#b8b8b8]">
+        <div className="text-[18px] leading-[27px] text-[#b8b8b8] mb:text-[16px]">
           Copyright © 2024 KDSC - All Rights Reserved
         </div>
-        <div className="flex items-center justify-center gap-[28px]">
+        <div className="flex items-center justify-center gap-[28px] mb:hidden">
           <div className="flex justify-start flex-col">
             <span className="text-[20px] leading-[27px] text-white font-normal">
               <Link className="underline" href={"https://festa.io/hosts/2003"}>
